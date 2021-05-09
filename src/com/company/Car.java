d6f99b889bfdd9a9587704143f745e743189247c
Jedrzej Robaczewski <jedrzejrobaczewski@gmail.com>
2021-05-09 16:16:48 +0200

    zad 1.1

---
+++ src/com/company/Car.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Car {
+    final String model;
+    final String producer;
+
+    public Car(String model, String producer)
+    {
+        this.producer=producer;
+        this.model=model;
+    }
+
+}
